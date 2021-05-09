2c3ab27252d885340169301a1152a020092f0cdc
EC2 Default User <ec2-user@ip-172-31-30-92.us-east-2.compute.internal>
2021-05-09 12:10:26 +0000

    Second commit

--- memo_2.rb
+++ memo_2.rb
@@ -1,23 +1,40 @@
-def direction
- directions = ["上", "下", "左", "右"]
- puts "-------------------------"
- puts "あっちむいて〜"
- puts "0(上) 1(下) 2(左) 3(右)"
- @my_direction = gets.chomp.to_i
- @opponent_direction = rand(4)
- puts "ホイ!"
- puts "-------------------------"
- puts "あなた:#{directions[@my_direction]}"
- puts "相手:#{directions[@opponent_direction]}"
+ def direction
+  directions = ["上", "下", "左", "右"]
+  puts "-------------------------"
+  puts "あっちむいて〜"
+  puts "0(上) 1(下) 2(左) 3(右)"
+  @my_direction = gets.chomp.to_i
+  @opponent_direction = rand(4)
+  puts "ホイ!"
+  puts "-------------------------"
+  puts "あなた:#{directions[@my_direction]}"
+  puts "相手:#{directions[@opponent_direction]}"
+ end
+ 
+ def direction_lose
+ direction
+  if  (@my_direction == @opponent_direction)
+   puts "YOU LOSE!"
+   exit
+  else
+   puts "あいこで..."
+   return true
+  end
+  return true
+ end
+
+def direction_win
+ direction
+  if  (@my_direction == @opponent_direction)
+   puts "YOU WIN!"
+   exit
+  else
+   puts "あいこで..."
+   return true
+  end
+   return true
 end
 
-def result
- puts "ホイ！"
- puts "-------------------------"
- puts "あなた:#{@choices[@my_choice]}を出しました"
- puts "相手:#{@choices[@opponent_choise]}を出しました"
- puts "-------------------------"
-end
 # じゃんけんの処理
 def janken
  puts "じゃんけん..."
@@ -25,32 +42,33 @@ def janken
  @choices = ["グー", "チョキ", "パー"]
  @my_choice = gets.to_i
  @opponent_choise = rand(3)
+ puts "-------------------------"
+ puts "あなた：#{@choices[@my_choice]}"
+ puts "相手：#{@choices[@opponent_choise]}"
 
  # 選択肢以外を選択した場合は予めループするようにする
  if @my_choice > 3
- puts "0~3の中から選択してください"
- puts "-------------------------"
- return true
+  puts "0~3の中から選択してください"
+  puts "-------------------------"
+  return true
  end
 
  if @my_choice == 3
- puts "じゃんけんを終了します"
- return false
+  puts "じゃんけんを終了します"
+  return false
  elsif (@my_choice == @opponent_choise)
- puts "あいこで..."
- return true
+  puts "あいこで..."
+  return true
  elsif (@my_choice == 0 && @opponent_choise ==2) ||
- (@my_choice == 1 && @opponent_choise ==0) ||
- (@my_choice == 2 && @opponent_choise ==1)
+  (@my_choice == 1 && @opponent_choise ==0) ||
+  (@my_choice == 2 && @opponent_choise ==1)
   puts "YOU LOSE!"
-  direction
- return true
+  direction_lose
  else 
   puts "YOU WIN!"
-   direction
- return true
+  direction_win
  end
- return true
+  return true
 end
 
 # janken
